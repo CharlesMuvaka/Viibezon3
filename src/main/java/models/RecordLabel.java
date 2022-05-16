26f3f6fd3455d2cc5ba64448f608386aab7dcc49
name <email>
2022-05-16 16:47:17 +0300

    ft: added features

--- src/main/java/models/RecordLabel.java
+++ src/main/java/models/RecordLabel.java
@@ -1,4 +1,33 @@
 package models;
 
 public class RecordLabel {
+    private int id;
+    private String name;
+    private String location;
+
+
+    public RecordLabel(String name, String location  ) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
