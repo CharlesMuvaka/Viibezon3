a8710f2807865cb2c442b37204d105d3c5bfa679
name <collins.odinga@student.moringaschool.com>
2022-05-17 13:18:22 +0300

    ft:added features

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@ import models.Musician;
 public class App {
     public static void main(String[] args) {
         sql2oMusicianDao dao = new sql2oMusicianDao();
-        Musician musician = new Musician("Tim","Trumpet","Instrumentalist",1);
+        Musician musician = new Musician("Tim","Trumpet","Instrumentalist",1 ,"Rock");
         dao.add(musician);
     }
 }
