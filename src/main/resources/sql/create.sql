0778a00255f6c924dbfbf0561bf6c52548d93430
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-18 09:04:59 +0300

    Insert default values in the musicians and record_label table

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -10,6 +10,11 @@ CREATE TABLE musicians(
  genre VARCHAR
 );
 
+INSERT INTO musicians(name, instrument, artistType, recordLabelId, avatarUrl, genre) VALUES('Levi Dunk','Violin','Vocalist', 1, '/images/lack.webp','GengeTone');
+INSERT INTO musicians(name, instrument, artistType, recordLabelId, avatarUrl, genre) VALUES('KoKoKolinko','Drums','Dancer' ,  4 ,'/images/darmian2.jpeg','Jazz');
+INSERT INTO musicians(name, instrument, artistType, recordLabelId, avatarUrl, genre) VALUES('Knives Katana','Guitar','Guitarist', 2,'/images/shans.jpg','Country Music' );
+INSERT INTO musicians(name, instrument, artistType, recordLabelId, avatarUrl, genre) VALUES('Muvkin Kid', 'Trumpets', 'Vocalist', 3, '/images/jj.jpeg', 'Electronic Dance Music');
+
 CREATE TABLE songs(
  id serial PRIMARY KEY,
 title VARCHAR,
@@ -24,3 +29,8 @@ location VARCHAR,
 manager VARCHAR,
 avatarUrl VARCHAR
 );
+
+INSERT INTO record_labels(name, location, manager, avatarUrl ) VALUES('Black Market Record', 'Nepal', 'Levi Opunga', '/images/label3.png');
+INSERT INTO record_labels(name, location, manager, avatarUrl ) VALUES('Sony Entertainment', 'Umoja ', 'Collins Odinga', '/images/img5.webp');
+INSERT INTO record_labels(name, location, manager, avatarUrl ) VALUES('Blue Inc Corp', 'Lagos', 'Joan Ndeng', '/images/label6.jpeg');
+INSERT INTO record_labels(name, location, manager, avatarUrl ) VALUES('After-Math', 'Berlin', 'Muvkin', '/images/stud1.jpeg');
